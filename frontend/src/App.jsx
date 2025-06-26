3274866291097fbfd4ab2671cbced203662fec8b
Anish Shukla <shuklaanish040@gmail.com>
2025-06-26 19:07:25 +0530

    auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,14 @@
 import { useState } from 'react'
+import {ClerkProviderWithRoutes} from "./auth/ClerkProviderWithRoutes.jsx"
+import {Routes, Route} from "react-router-dom"
 import './App.css'
 
 function App() {
-  return <></>
+    return <ClerkProviderWithRoutes>
+        <Routes>
+
+        </Routes>
+    </ClerkProviderWithRoutes>
 }
 
 export default App
