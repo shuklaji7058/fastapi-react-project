63c1a9a871397ce4b01ea0efc2ac5eb577cbf354
Anish Shukla <shuklaanish040@gmail.com>
2025-06-26 19:52:09 +0530

    auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 import {ClerkProviderWithRoutes} from "./auth/ClerkProviderWithRoutes.jsx"
 import {Routes, Route} from "react-router-dom"
+import {Layout} from "./layout/Layout.jsx"
+import {HistoryPanel} from "./history/HistoryPanel.jsx"
+import {ChallengeGenerator} from "./challenge/ChallengeGenerator.jsx"
+import {AuthenticationPage} from "./auth/AuthenticationPage.jsx"
 import './App.css'
 
 function App() {
