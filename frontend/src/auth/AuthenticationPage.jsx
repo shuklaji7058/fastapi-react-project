9ed5e1d94bb2829fa7052fc41c12d7f98d4c284d
Anish Shukla <shuklaanish040@gmail.com>
2025-06-27 12:45:17 +0530

    auth

--- frontend/src/auth/AuthenticationPage.jsx
+++ frontend/src/auth/AuthenticationPage.jsx
@@ -1,5 +1,16 @@
 import "react"
+import {SignIn, SignUp, SignedIn, SignedOut} from "@clerk/clerk-react"
 
 export function AuthenticationPage() {
-    return <></>
-    }
+    return <div className="auth-container">
+        <SignedOut>
+            <SignIn routing="path" path="/sign-in"/>
+            <SignUp routing="path" path="/sign-up"/>
+        </SignedOut>
+        <SignedIn>
+            <div className="redirect-message">
+                <p>You are already signed in.</p>
+            </div>
+        </SignedIn>
+    </div>
+}
